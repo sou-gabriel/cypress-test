f16be3ebb6651e4b46151a8956a5d3869e248719
Gabriel Ramos <dev.gabrielramos@gmail.com>
2021-12-16 00:45:07 -0300

    feat: Update the command naming and create a new user login command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -41,3 +41,40 @@ Cypress.Commands.add('login', () => {
     localStorage.setItem('token', token)
   })
 })
+
+Cypress.Commands.add('signup', (values, expectedStatus) => {
+  cy.get(':nth-child(1) > .sc-hKwDye').type(values.username)
+  cy.get(':nth-child(2) > .sc-hKwDye').type(values.email)
+  cy.get(':nth-child(3) > .sc-hKwDye').type(values.password)
+
+  cy.intercept({
+    method: 'POST',
+    url: `${Cypress.env('endpoint')}/user/create`,
+  }).as('registerUser')
+  
+  cy.get('.sc-jRQBWg').click()
+  
+  cy.wait('@registerUser').then(request => {
+    cy.log(request)
+    expect(request.response.statusCode).to.be.eq(expectedStatus)
+  })
+})
+
+Cypress.Commands.add('signin', (values, expectedStatus) => {
+  cy.get(':nth-child(1) > .sc-hKwDye').type(values.email)
+  cy.get(':nth-child(2) > .sc-hKwDye').type(values.password)
+
+  cy.intercept({
+    method: 'POST',
+    url: `${Cypress.env('endpoint')}/login`,
+  }).as('login')
+
+  cy.get('.sc-jRQBWg').click()
+
+  cy.wait('@login').then(request => {
+    cy.log(request)
+    expect(request.response.statusCode).to.be.eq(expectedStatus)
+  })
+})
+
+
